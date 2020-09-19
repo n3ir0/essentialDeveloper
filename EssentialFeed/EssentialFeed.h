dde31189a657e2d619f54a52412fd9d5a23c105d
Evgeny Gabelev <neur0@me.com>
2020-09-19 16:27:51 +0300

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Evgeny Gabelev on 19.09.2020.
+//  Copyright © 2020 Evgeny Gabelev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
