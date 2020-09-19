7393599ceaa80eb22007952ccbdf7a912c2460fc
Evgeny Gabelev <neur0@me.com>
2020-09-19 17:11:02 +0300

    created FeedLoader protocol

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,18 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Evgeny Gabelev on 19.09.2020.
+//  Copyright © 2020 Evgeny Gabelev. All rights reserved.
+//
+
+import Foundation
+
+enum LoadFeedResult {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
