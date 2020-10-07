eda169b7f0a6ac4f314fdbce98429c636a94abf9
Evgeny GABELEV <Evgeny.GABELEV@raiffeisen.ru>
2020-10-07 18:41:13 +0300

    create test_getFromURL_createsDataTaskWithURL

---
+++ EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -0,0 +1,49 @@
+//
+//  URLSessionHTTPClientTests.swift
+//  EssentialFeedTests
+//
+//  Created by GABELEV Evgeny on 07.10.2020.
+//  Copyright © 2020 Evgeny Gabelev. All rights reserved.
+//
+
+import XCTest
+
+class URLSessionHTTPClient {
+    private let session: URLSession
+    
+    init(session: URLSession) {
+        self.session = session
+    }
+    
+    func get(from url: URL) {
+        session.dataTask(with: url) { (_, _, _) in
+            
+        }
+    }
+}
+
+class URLSessionHTTPClientTests: XCTestCase {
+
+    func test_getFromURL_createsDataTaskWithURL() {
+        let url = URL(string: "https://any-url.com")!
+        let session = URLSessionSpy()
+        let sut = URLSessionHTTPClient(session: session)
+        
+        sut.get(from: url)
+        
+        XCTAssertEqual(session.receivedURLs, [url])
+    }
+    
+    // MARK: - Helpers
+    
+    private class URLSessionSpy: URLSession {
+        var receivedURLs = [URL]()
+        
+        override func dataTask(with url: URL, completionHandler: @escaping (Data?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
+            receivedURLs.append(url)
+            return FakeURLSessionDataTask()
+        }
+    }
+    
+    private class FakeURLSessionDataTask: URLSessionDataTask {}
+}
