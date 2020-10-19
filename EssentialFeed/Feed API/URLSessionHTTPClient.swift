a0973056b5a99620d067c11a39c01ba137d512ae
Evgeny GABELEV <Evgeny.GABELEV@raiffeisen.ru>
2020-10-19 17:35:47 +0300

    moved URLSessionHTTPClient to its own file

---
+++ EssentialFeed/Feed API/URLSessionHTTPClient.swift
@@ -0,0 +1,32 @@
+//
+//  URLSessionHTTPClient.swift
+//  EssentialFeed
+//
+//  Created by GABELEV Evgeny on 19.10.2020.
+//  Copyright © 2020 Evgeny Gabelev. All rights reserved.
+//
+
+import Foundation
+
+public class URLSessionHTTPClient: HTTPClient {
+    private let session: URLSession
+    
+    public init(session: URLSession = .shared) {
+        self.session = session
+    }
+    
+    private struct UnexpectedValuesRepresentation: Error {
+    }
+    
+    public func get(from url: URL, completion: @escaping (HTTPClientResult) -> Void) {
+        session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                completion(.failure(error))
+            } else if let data = data, let response = response as? HTTPURLResponse {
+                completion(.success(data, response))
+            } else {
+                completion(.failure(UnexpectedValuesRepresentation()))
+            }
+        }.resume()
+    }
+}
